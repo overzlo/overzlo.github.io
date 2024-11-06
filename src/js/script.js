bdce67d8acf40452072bae756bb182e446af3fd5
overzlo <flashzed_969@mail.ru>
2024-11-07 04:40:59 +0500

    ә

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// Функция для открытия модального окна
+function openModal(img) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal_image");
+    const captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src; // Устанавливаем изображение из галереи
+    captionText.innerHTML = img.alt; // Устанавливаем описание из атрибута alt
+}
+
+// Функция для закрытия модального окна
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
